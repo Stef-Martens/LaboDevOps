6cf537f0a0c27af07e0f081e38319136abe1d65b
Estee <helgabuchhausel@gmail.com>
2019-10-16 14:42:59 +0200

    basic buttons for the games

--- PM1918_Project/PM1918_Project/Form1.cs
+++ PM1918_Project/PM1918_Project/Form1.cs
@@ -16,5 +16,20 @@ namespace PM1918_Project
         {
             InitializeComponent();
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+
+        }
     }
 }
