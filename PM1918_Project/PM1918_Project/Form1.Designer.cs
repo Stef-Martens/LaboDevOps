58041332c7c92587d1a6e431d4b2dedf8ccbc911
Estee <helgabuchhausel@gmail.com>
2019-10-17 10:24:24 +0200

    return button

--- PM1918_Project/PM1918_Project/Form1.Designer.cs
+++ PM1918_Project/PM1918_Project/Form1.Designer.cs
@@ -28,49 +28,61 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.button1 = new System.Windows.Forms.Button();
-            this.button2 = new System.Windows.Forms.Button();
-            this.button3 = new System.Windows.Forms.Button();
+            this.snake_button = new System.Windows.Forms.Button();
+            this.bss_button = new System.Windows.Forms.Button();
+            this.pong_button = new System.Windows.Forms.Button();
+            this.back_to_main = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
-            // button1
+            // snake_button
             // 
-            this.button1.Location = new System.Drawing.Point(300, 190);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(130, 23);
-            this.button1.TabIndex = 0;
-            this.button1.Text = "Snake";
-            this.button1.UseVisualStyleBackColor = true;
-            this.button1.Click += new System.EventHandler(this.button1_Click);
+            this.snake_button.Location = new System.Drawing.Point(300, 190);
+            this.snake_button.Name = "snake_button";
+            this.snake_button.Size = new System.Drawing.Size(130, 23);
+            this.snake_button.TabIndex = 0;
+            this.snake_button.Text = "Snake";
+            this.snake_button.UseVisualStyleBackColor = true;
+            this.snake_button.Click += new System.EventHandler(this.button1_Click);
             // 
-            // button2
+            // bss_button
             // 
-            this.button2.Location = new System.Drawing.Point(300, 234);
-            this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(130, 23);
-            this.button2.TabIndex = 1;
-            this.button2.Text = "Blad-Steen-Schaar";
-            this.button2.UseVisualStyleBackColor = true;
-            this.button2.Click += new System.EventHandler(this.button2_Click);
+            this.bss_button.Location = new System.Drawing.Point(300, 234);
+            this.bss_button.Name = "bss_button";
+            this.bss_button.Size = new System.Drawing.Size(130, 23);
+            this.bss_button.TabIndex = 1;
+            this.bss_button.Text = "Blad-Steen-Schaar";
+            this.bss_button.UseVisualStyleBackColor = true;
+            this.bss_button.Click += new System.EventHandler(this.button2_Click);
             // 
-            // button3
+            // pong_button
             // 
-            this.button3.Location = new System.Drawing.Point(300, 278);
-            this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(130, 23);
-            this.button3.TabIndex = 2;
-            this.button3.Text = "Pong";
-            this.button3.UseVisualStyleBackColor = true;
-            this.button3.Click += new System.EventHandler(this.button3_Click);
+            this.pong_button.Location = new System.Drawing.Point(300, 278);
+            this.pong_button.Name = "pong_button";
+            this.pong_button.Size = new System.Drawing.Size(130, 23);
+            this.pong_button.TabIndex = 2;
+            this.pong_button.Text = "Pong";
+            this.pong_button.UseVisualStyleBackColor = true;
+            this.pong_button.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // back_to_main
+            // 
+            this.back_to_main.Location = new System.Drawing.Point(300, 378);
+            this.back_to_main.Name = "back_to_main";
+            this.back_to_main.Size = new System.Drawing.Size(130, 23);
+            this.back_to_main.TabIndex = 3;
+            this.back_to_main.Text = "return to main menu";
+            this.back_to_main.UseVisualStyleBackColor = true;
+            this.back_to_main.Click += new System.EventHandler(this.button1_Click_1);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Controls.Add(this.button3);
-            this.Controls.Add(this.button2);
-            this.Controls.Add(this.button1);
+            this.Controls.Add(this.back_to_main);
+            this.Controls.Add(this.pong_button);
+            this.Controls.Add(this.bss_button);
+            this.Controls.Add(this.snake_button);
             this.Name = "Form1";
             this.Text = "Form1";
             this.ResumeLayout(false);
@@ -79,9 +91,10 @@
 
         #endregion
 
-        private System.Windows.Forms.Button button1;
-        private System.Windows.Forms.Button button2;
-        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button snake_button;
+        private System.Windows.Forms.Button bss_button;
+        private System.Windows.Forms.Button pong_button;
+        private System.Windows.Forms.Button back_to_main;
     }
 }
 
--- PM1918_Project/PM1918_Project/Form1.cs
+++ PM1918_Project/PM1918_Project/Form1.cs
@@ -31,5 +31,10 @@ namespace PM1918_Project
         {
 
         }
+
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+
+        }
     }
 }
